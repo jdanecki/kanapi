246a59aaa38712d1355da0f45e9d4ff719468ce8
Jacek Danecki <jacek.m.danecki@gmail.com>
2012-12-27 20:40:08 +0000

    pygtk added
    - pygtk-demo works, run it by pygtk-demo.sh
    
    Signed-off-by: Jacek Danecki <jacek.m.danecki@gmail.com>

---
+++ kanapi_base_system/scripts/pygtk/pygtk-demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONPATH=/kanapi_packages/pygtk-2.24.0/lib/python2.7/site-packages/gtk-2.0:/kanapi_packages/pygobject-2.28.6/lib/python2.7/site-packages:/kanapi_packages/py2cairo-1.10.0/lib/python2.7/site-packages:/kanapi_packages/pygobject-2.28.6/lib/python2.7/site-packages/gtk-2.0/
+pygtk-demo
+
