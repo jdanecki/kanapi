a12f6bfd3e5079faaa4b6c89190093700d7d23ce
Jacek Danecki <jacek.m.danecki@gmail.com>
2016-12-05 01:57:20 +0100

    gcc: unit test for long size
    Signed-off-by: Jacek Danecki <jacek.m.danecki@gmail.com>

---
+++ kanapi_system/tests/test_long.c
@@ -0,0 +1,11 @@
+#include <limits.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("SSIZE_MAX = %ld\n", SSIZE_MAX);
+	printf("sizeof(long) = %ld\n", sizeof(long));
+	return 0;
+}
+
